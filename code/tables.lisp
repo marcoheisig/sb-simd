be12acbf48d72ee838169c802a20366fa67a5211
Marco Heisig <marco.heisig@fau.de>
2020-10-23 23:19:26 +0200

    Properly indent all tables.

--- code/tables.lisp
+++ code/tables.lisp
@@ -35,22 +35,22 @@
 
 (defparameter *scalar-records*
   (loop
-    for (name bits type primitive-type register)
-      in '(( u1  1 (unsigned-byte  1) (unsigned-byte  1) sb-vm::unsigned-reg)
-           ( u2  2 (unsigned-byte  2) (unsigned-byte  2) sb-vm::unsigned-reg)
-           ( u4  4 (unsigned-byte  4) (unsigned-byte  4) sb-vm::unsigned-reg)
-           ( u8  8 (unsigned-byte  8) (unsigned-byte  8) sb-vm::unsigned-reg)
-           (u16 16 (unsigned-byte 16) (unsigned-byte 16) sb-vm::unsigned-reg)
-           (u32 32 (unsigned-byte 32) (unsigned-byte 32) sb-vm::unsigned-reg)
-           (u64 64 (unsigned-byte 64) (unsigned-byte 64) sb-vm::unsigned-reg)
-           ( s8  8 (signed-byte  8) (signed-byte  8) sb-vm::signed-reg)
-           (s16 16 (signed-byte 16) (signed-byte 16) sb-vm::signed-reg)
-           (s32 32 (signed-byte 32) (signed-byte 32) sb-vm::signed-reg)
-           (s64 64 (signed-byte 64) (signed-byte 64) sb-vm::signed-reg)
-           (f32 32 single-float single-float sb-vm::single-reg)
-           (f64 64 double-float double-float sb-vm::double-reg)
-           (c64 64 (complex single-float) sb-kernel::complex-single-float sb-vm::complex-single-reg)
-           (c128 128 (complex double-float) sb-kernel::complex-double-float sb-vm::complex-double-reg))
+    for    (name bits type                   primitive-type                  register)
+      in '((  u1    1 (unsigned-byte  1)     (unsigned-byte  1)              sb-vm::unsigned-reg)
+           (  u2    2 (unsigned-byte  2)     (unsigned-byte  2)              sb-vm::unsigned-reg)
+           (  u4    4 (unsigned-byte  4)     (unsigned-byte  4)              sb-vm::unsigned-reg)
+           (  u8    8 (unsigned-byte  8)     (unsigned-byte  8)              sb-vm::unsigned-reg)
+           ( u16   16 (unsigned-byte 16)     (unsigned-byte 16)              sb-vm::unsigned-reg)
+           ( u32   32 (unsigned-byte 32)     (unsigned-byte 32)              sb-vm::unsigned-reg)
+           ( u64   64 (unsigned-byte 64)     (unsigned-byte 64)              sb-vm::unsigned-reg)
+           (  s8    8 (signed-byte  8)       (signed-byte  8)                sb-vm::signed-reg)
+           ( s16   16 (signed-byte 16)       (signed-byte 16)                sb-vm::signed-reg)
+           ( s32   32 (signed-byte 32)       (signed-byte 32)                sb-vm::signed-reg)
+           ( s64   64 (signed-byte 64)       (signed-byte 64)                sb-vm::signed-reg)
+           ( f32   32 single-float           single-float                    sb-vm::single-reg)
+           ( f64   64 double-float           double-float                    sb-vm::double-reg)
+           ( c64   64 (complex single-float) sb-kernel::complex-single-float sb-vm::complex-single-reg)
+           (c128  128 (complex double-float) sb-kernel::complex-double-float sb-vm::complex-double-reg))
     collect
     (make-scalar-record
      :name name
@@ -79,13 +79,13 @@
 
 (defparameter *simd-records*
   (loop
-    for (name scalar-record-name size bits type primitive-type register packer primitive-packer unpacker primitive-unpacker)
-      in '((u64.2 u64 2 128 (sb-ext:simd-pack (unsigned-byte 64)) sb-kernel:simd-pack-int sb-vm::int-sse-reg make-u64.2 sb-ext:%make-simd-pack-ub64 u64.2-values sb-ext:%simd-pack-ub64s)
-           (f32.4 f32 4 128 (sb-ext:simd-pack single-float) sb-kernel:simd-pack-single sb-vm::single-sse-reg make-f32.4 sb-ext:%make-simd-pack-single f32.4-values sb-ext:%simd-pack-singles)
-           (f64.2 f64 2 128 (sb-ext:simd-pack double-float) sb-kernel:simd-pack-double sb-vm::double-sse-reg make-f64.2 sb-ext:%make-simd-pack-double f64.2-values sb-ext:%simd-pack-doubles)
-           (u64.4 u64 4 256 (sb-ext:simd-pack-256 (unsigned-byte 64)) sb-kernel:simd-pack-256-int sb-vm::int-avx2-reg make-u64.4 sb-ext:%make-simd-pack-256-ub64 u64.4-values sb-ext:%simd-pack-256-ub64s)
-           (f32.8 f32 8 256 (sb-ext:simd-pack-256 single-float) sb-kernel:simd-pack-256-single sb-vm::single-avx2-reg make-f32.8 sb-ext:%make-simd-pack-256-single f32.8-values sb-ext:%simd-pack-256-singles)
-           (f64.4 f64 4 256 (sb-ext:simd-pack-256 double-float) sb-kernel:simd-pack-256-double sb-vm::double-avx2-reg make-f64.4 sb-ext:%make-simd-pack-256-double f64.4-values sb-ext:%simd-pack-256-doubles))
+    for    (name scalar-record-name size bits type                                      primitive-type                 register               packer     primitive-packer                  unpacker     primitive-unpacker)
+      in '((u64.2               u64    2  128 (sb-ext:simd-pack (unsigned-byte 64))     sb-kernel:simd-pack-int        sb-vm::int-sse-reg     make-u64.2 sb-ext:%make-simd-pack-ub64       u64.2-values sb-ext:%simd-pack-ub64s)
+           (f32.4               f32    4  128 (sb-ext:simd-pack single-float)           sb-kernel:simd-pack-single     sb-vm::single-sse-reg  make-f32.4 sb-ext:%make-simd-pack-single     f32.4-values sb-ext:%simd-pack-singles)
+           (f64.2               f64    2  128 (sb-ext:simd-pack double-float)           sb-kernel:simd-pack-double     sb-vm::double-sse-reg  make-f64.2 sb-ext:%make-simd-pack-double     f64.2-values sb-ext:%simd-pack-doubles)
+           (u64.4               u64    4  256 (sb-ext:simd-pack-256 (unsigned-byte 64)) sb-kernel:simd-pack-256-int    sb-vm::int-avx2-reg    make-u64.4 sb-ext:%make-simd-pack-256-ub64   u64.4-values sb-ext:%simd-pack-256-ub64s)
+           (f32.8               f32    8  256 (sb-ext:simd-pack-256 single-float)       sb-kernel:simd-pack-256-single sb-vm::single-avx2-reg make-f32.8 sb-ext:%make-simd-pack-256-single f32.8-values sb-ext:%simd-pack-256-singles)
+           (f64.4               f64    4  256 (sb-ext:simd-pack-256 double-float)       sb-kernel:simd-pack-256-double sb-vm::double-avx2-reg make-f64.4 sb-ext:%make-simd-pack-256-double f64.4-values sb-ext:%simd-pack-256-doubles))
     for scalar-record = (find scalar-record-name *scalar-records* :key #'scalar-record-name)
     when (sb-ext:valid-type-specifier-p type)
       collect
@@ -179,28 +179,28 @@
 
 (defparameter *instruction-records*
   (loop
-    for (name mnemonic result-records argument-records . attributes)
+    for    (name             mnemonic  result-records argument-records . attributes)
       in '(;; Casts
-           (f32.4-from-f64.4 vcvtpd2ps (f32.4) (f64.4) :cost 5)
-           (f64.4-from-f32.4 vcvtps2pd (f64.4) (f32.4) :cost 5)
+           (f32.4-from-f64.4 vcvtpd2ps (f32.4)        (f64.4)            :cost 5)
+           (f64.4-from-f32.4 vcvtps2pd (f64.4)        (f32.4)            :cost 5)
            ;; SSE arithmetic operations
-           (f64.2-two-arg-+ addpd (f64.2) (f64.2 f64.2) :cost 2 :first-arg-stores-result t :commutative t)
-           (f64.2-two-arg-- subpd (f64.2) (f64.2 f64.2) :cost 2 :first-arg-stores-result t)
-           (f64.2-two-arg-* mulpd (f64.2) (f64.2 f64.2) :cost 2 :first-arg-stores-result t :commutative t)
-           (f64.2-two-arg-/ divpd (f64.2) (f64.2 f64.2) :cost 8 :first-arg-stores-result t)
-           (f32.4-two-arg-+ addps (f32.4) (f32.4 f32.4) :cost 2 :first-arg-stores-result t :commutative t)
-           (f32.4-two-arg-- subds (f32.4) (f32.4 f32.4) :cost 2 :first-arg-stores-result t)
-           (f32.4-two-arg-* mulps (f32.4) (f32.4 f32.4) :cost 2 :first-arg-stores-result t :commutative t)
-           (f32.4-two-arg-/ divps (f32.4) (f32.4 f32.4) :cost 8 :first-arg-stores-result t)
+           (f64.2-two-arg-+  addpd     (f64.2)        (f64.2 f64.2)      :cost 2 :first-arg-stores-result t :commutative t)
+           (f64.2-two-arg--  subpd     (f64.2)        (f64.2 f64.2)      :cost 2 :first-arg-stores-result t)
+           (f64.2-two-arg-*  mulpd     (f64.2)        (f64.2 f64.2)      :cost 2 :first-arg-stores-result t :commutative t)
+           (f64.2-two-arg-/  divpd     (f64.2)        (f64.2 f64.2)      :cost 8 :first-arg-stores-result t)
+           (f32.4-two-arg-+  addps     (f32.4)        (f32.4 f32.4)      :cost 2 :first-arg-stores-result t :commutative t)
+           (f32.4-two-arg--  subds     (f32.4)        (f32.4 f32.4)      :cost 2 :first-arg-stores-result t)
+           (f32.4-two-arg-*  mulps     (f32.4)        (f32.4 f32.4)      :cost 2 :first-arg-stores-result t :commutative t)
+           (f32.4-two-arg-/  divps     (f32.4)        (f32.4 f32.4)      :cost 8 :first-arg-stores-result t)
            ;; AVX2 arithmetic operations
-           (f64.4-two-arg-+ vaddpd (f64.4) (f64.4 f64.4) :cost 2 :commutative t)
-           (f64.4-two-arg-- vsubpd (f64.4) (f64.4 f64.4) :cost 2)
-           (f64.4-two-arg-* vmulpd (f64.4) (f64.4 f64.4) :cost 2 :commutative t)
-           (f64.4-two-arg-/ vdivpd (f64.4) (f64.4 f64.4) :cost 8)
-           (f32.8-two-arg-+ vaddps (f32.8) (f32.8 f32.8) :cost 2 :commutative t)
-           (f32.8-two-arg-- vsubps (f32.8) (f32.8 f32.8) :cost 2)
-           (f32.8-two-arg-* vmulps (f32.8) (f32.8 f32.8) :cost 2 :commutative t)
-           (f32.8-two-arg-/ vdivps (f32.8) (f32.8 f32.8) :cost 8))
+           (f64.4-two-arg-+  vaddpd    (f64.4)        (f64.4 f64.4)      :cost 2 :commutative t)
+           (f64.4-two-arg--  vsubpd    (f64.4)        (f64.4 f64.4)      :cost 2)
+           (f64.4-two-arg-*  vmulpd    (f64.4)        (f64.4 f64.4)      :cost 2 :commutative t)
+           (f64.4-two-arg-/  vdivpd    (f64.4)        (f64.4 f64.4)      :cost 8)
+           (f32.8-two-arg-+  vaddps    (f32.8)        (f32.8 f32.8)      :cost 2 :commutative t)
+           (f32.8-two-arg--  vsubps    (f32.8)        (f32.8 f32.8)      :cost 2)
+           (f32.8-two-arg-*  vmulps    (f32.8)        (f32.8 f32.8)      :cost 2 :commutative t)
+           (f32.8-two-arg-/  vdivps    (f32.8)        (f32.8 f32.8)      :cost 8))
     when (find-symbol (string mnemonic) sb-assem::*backend-instruction-set-package*)
       collect
       (make-instruction-record
