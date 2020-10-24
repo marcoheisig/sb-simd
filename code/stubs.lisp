4a8b92de7e972b8357e75bcdc89848175fd2bb73
Marco Heisig <marco.heisig@fau.de>
2020-10-24 08:36:54 +0200

    Don't export stubs.

--- code/stubs.lisp
+++ code/stubs.lisp
@@ -5,15 +5,17 @@
                    (argument-records instruction-record-argument-records)
                    (result-records instruction-record-result-records))
       (find-instruction-record-by-name instruction-record-name)
-    (export name)
     (let ((arguments (subseq *arguments* 0 (length argument-records)))
           (vop-name (vop-name name)))
       `(progn
+         ;; Define a function of the same name as the VOP.
          (defun ,vop-name ,arguments
            ,@(loop for argument in arguments
                    for type in (mapcar #'value-record-name argument-records)
                    collect `(declare (type ,type ,argument)))
            (,vop-name ,@arguments))
+         ;; Define a high-level wrapper function that attempts to cast all
+         ;; arguments to the correct types.
          (define-inline ,name ,arguments
            (,vop-name
             ,@(loop for argument in arguments
