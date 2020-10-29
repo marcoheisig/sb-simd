b780b16984756704edd1c2985d12793ee39e6333
Marco Heisig <marco.heisig@fau.de>
2020-10-29 10:27:28 +0100

    Rewrite define-nary-wrapper* so that we don't need one-arg-fns.

--- code/wrappers.lisp
+++ code/wrappers.lisp
@@ -43,15 +43,14 @@
 (define-nary-wrapper f32.8+ f32.8 two-arg-f32.8-+ (make-f32.8 0f0 0f0 0f0 0f0 0f0 0f0 0f0 0f0))
 (define-nary-wrapper f32.8* f32.8 two-arg-f32.8-* (make-f32.8 1f0 1f0 1f0 1f0 1f0 1f0 1f0 1f0))
 
-(defmacro define-nary-wrapper* (name simd-type two-arg-fn one-arg-fn)
+(defmacro define-nary-wrapper* (name simd-type two-arg-fn neutral-element)
   (export name)
   (if (and (value-record-name-p simd-type)
-           (instruction-record-name-p two-arg-fn)
-           (instruction-record-name-p one-arg-fn))
+           (instruction-record-name-p two-arg-fn))
       `(progn
          (defun ,name (arg &rest more-args)
            (if (null more-args)
-               (,one-arg-fn (,simd-type arg))
+               (,two-arg-fn ,neutral-element (,simd-type arg))
                (let ((acc (,simd-type arg)))
                  (declare (,simd-type acc))
                  (loop for arg in more-args do
@@ -59,7 +58,7 @@
                  acc)))
          (define-compiler-macro ,name (arg &rest more-args)
            (cond ((null more-args)
-                  `(,',one-arg-fn (,',simd-type ,(first args))))
+                  `(,',two-arg-fn ,',neutral-element (,',simd-type ,(first args))))
                  (t (reduce
                      (lambda (a b) `(,',two-arg-fn a (,',simd-type ,b)))
                      args
@@ -70,16 +69,16 @@
                 ',name))))
 
 ;; 128 bit instructions
-(define-nary-wrapper* u64.2- u64.2 two-arg-u64.2-- one-arg-u64.2--)
-(define-nary-wrapper* u64.2/ u64.2 two-arg-u64.2-/ one-arg-u64.2-/)
-(define-nary-wrapper* f64.2- f64.2 two-arg-f64.2-- one-arg-f64.2--)
-(define-nary-wrapper* f64.2/ f64.2 two-arg-f64.2-/ one-arg-f64.2-/)
-(define-nary-wrapper* f32.4- f32.4 two-arg-f32.4-- one-arg-f32.4--)
-(define-nary-wrapper* f32.4/ f32.4 two-arg-f32.4-/ one-arg-f32.4-/)
+(define-nary-wrapper* u64.2- u64.2 two-arg-u64.2-- (make-u64.2 0 0))
+(define-nary-wrapper* u64.2/ u64.2 two-arg-u64.2-/ (make-u64.2 1 1))
+(define-nary-wrapper* f64.2- f64.2 two-arg-f64.2-- (make-f64.2 0d0 0d0))
+(define-nary-wrapper* f64.2/ f64.2 two-arg-f64.2-/ (make-f64.2 1d0 1d0))
+(define-nary-wrapper* f32.4- f32.4 two-arg-f32.4-- (make-f32.4 0f0 0f0 0f0 0f0))
+(define-nary-wrapper* f32.4/ f32.4 two-arg-f32.4-/ (make-f32.4 1f0 1f0 1f0 1f0))
 ;; 256 bit instructions
-(define-nary-wrapper* u64.4- u64.4 two-arg-u64.4-- one-arg-u64.4--)
-(define-nary-wrapper* u64.4/ u64.4 two-arg-u64.4-/ one-arg-u64.4-/)
-(define-nary-wrapper* f64.4- f64.4 two-arg-f64.4-- one-arg-f64.4--)
-(define-nary-wrapper* f64.4/ f64.4 two-arg-f64.4-/ one-arg-f64.4-/)
-(define-nary-wrapper* f32.8- f32.8 two-arg-f32.8-- one-arg-f32.8--)
-(define-nary-wrapper* f32.8/ f32.8 two-arg-f32.8-/ one-arg-f32.8-/)
+(define-nary-wrapper* u64.4- u64.4 two-arg-u64.4-- (make-u64.4 0 0 0 0))
+(define-nary-wrapper* u64.4/ u64.4 two-arg-u64.4-/ (make-u64.4 1 1 1 1))
+(define-nary-wrapper* f64.4- f64.4 two-arg-f64.4-- (make-f64.4 0d0 0d0 0d0 0d0))
+(define-nary-wrapper* f64.4/ f64.4 two-arg-f64.4-/ (make-f64.4 1d0 1d0 1d0 1d0))
+(define-nary-wrapper* f32.8- f32.8 two-arg-f32.8-- (make-f32.8 0f0 0f0 0f0 0f0 0f0 0f0 0f0 0f0))
+(define-nary-wrapper* f32.8/ f32.8 two-arg-f32.8-/ (make-f32.8 1f0 1f0 1f0 1f0 1f0 1f0 1f0 1f0))
