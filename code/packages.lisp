d721bd9b84142711cd8d6904dbbfb46d0c586ff9
Marco Heisig <marco.heisig@fau.de>
2021-05-27 11:24:57 +0200

    Explain the reader macro in the defpackage form.

--- code/packages.lisp
+++ code/packages.lisp
@@ -11,7 +11,10 @@
    #:+sse4.2+
    #:+avx+
    #:+avx2+
-   .
-   #.(when (find-package '#:sb-simd)
-       (loop for it being the external-symbols of '#:sb-simd
-             collect it))))
+   ;; Most functions in this library are generated automatically, and
+   ;; depend on the available instruction sets.  The following lines ensure
+   ;; that symbols that are exported once remain so, even when the
+   ;; defpackage form is re-evaluated.
+   . #.(when (find-package '#:sb-simd)
+         (loop for it being the external-symbols of '#:sb-simd
+               collect it))))
