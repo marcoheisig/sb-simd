e6d3ae2b4e5c5c4063b1cc9887d95bb6650d2841
Bela Pecsek <bela.pecsek@gmail.com>
2021-06-16 12:35:09 +0200

    More integer load and store instruction defined in instruction-set-table

--- code/unsigned-bytes.lisp
+++ code/unsigned-bytes.lisp
@@ -8,8 +8,8 @@
   (defun %make-simd-pack-ub16 (p0 p1 p2 p3 p4 p5 p6 p7)
     (declare (type (unsigned-byte 16) p0 p1 p2 p3 p4 p5 p6 p7))
     (%make-simd-pack-ub32
-     (logior p0 (ash p1 16)) (logior p2 (ash p3 16)) (logior p4 (ash p5 16))
-     (logior p6 (ash p7 16))))
+     (logior p0 (ash p1 16)) (logior p2 (ash p3 16))
+     (logior p4 (ash p5 16)) (logior p6 (ash p7 16))))
     (export '%make-simd-pack-ub16)
 
   (defun %simd-pack-ub16s (pack)
@@ -72,8 +72,8 @@
     (declare (type (unsigned-byte 8) p0 p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 p12
     p13 p14 p15 p16 p17 p18 p19 p20 p21 p22 p23 p24 p25 p26 p27 p28 p29 p30 p31))
     (%make-simd-pack-256-ub16
-     (logior p0  (ash p1  8)) (logior p2  (ash p3 8))  (logior p4  (ash p5 8))
-     (logior p6  (ash p7  8)) (logior p8  (ash p9 8))  (logior p10 (ash p11 8))
+     (logior p0  (ash p1  8)) (logior p2  (ash p3  8)) (logior p4  (ash p5  8))
+     (logior p6  (ash p7  8)) (logior p8  (ash p9  8)) (logior p10 (ash p11 8))
      (logior p12 (ash p13 8)) (logior p14 (ash p15 8)) (logior p16 (ash p17 8))
      (logior p18 (ash p19 8)) (logior p20 (ash p21 8)) (logior p22 (ash p23 8))
      (logior p24 (ash p25 8)) (logior p26 (ash p27 8)) (logior p28 (ash p29 8))
