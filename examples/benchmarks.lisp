d85dc92e17611717117658c53a8f55603768b3e9
Bela Pecsek <bpecsek@gmail.com>
2021-06-30 21:27:22 +0200

    improved speed of f64.4-fma-vdot in benchmark.lisp

--- examples/benchmarks.lisp
+++ examples/benchmarks.lisp
@@ -72,7 +72,8 @@
            ((>= index n) result)))))
 
 (declaim (inline f64.4-fma-vdot))
-(defun f64.4-fma-vdot (array1 array2 &aux (n (min (array-total-size array1) (array-total-size array2))))
+(defun f64.4-fma-vdot (array1 array2 &aux (n (min (array-total-size array1)
+                                                  (array-total-size array2))))
   (declare (optimize (speed 3) (safety 0) (debug 0))
            (type f64vec array1 array2))
   (let ((n0 (- n (mod n 16))))
@@ -81,14 +82,18 @@
              with acc3 of-type f64.4 = (f64.4 0)
              with acc4 of-type f64.4 = (f64.4 0)
              for i of-type fixnum below n0 by 16
-             do (setf acc1 (sb-simd-avx2:f64.4-fmadd231 acc1 (f64.4-aref array1 (+ i 0))
-                                                             (f64.4-aref array2 (+ i 0)))
-                      acc2 (sb-simd-avx2:f64.4-fmadd231 acc2 (f64.4-aref array1 (+ i 4))
-                                                             (f64.4-aref array2 (+ i 4)))
-                      acc3 (sb-simd-avx2:f64.4-fmadd231 acc3 (f64.4-aref array1 (+ i 8))
-                                                             (f64.4-aref array2 (+ i 8)))
-                      acc4 (sb-simd-avx2:f64.4-fmadd231 acc4 (f64.4-aref array1 (+ i 12))
-                                                             (f64.4-aref array2 (+ i 12))))
+             do (let ((array1-i0  (f64.4-aref array1 (+ i 0)))
+                      (array1-i4  (f64.4-aref array1 (+ i 4)))
+                      (array1-i8  (f64.4-aref array1 (+ i 8)))
+                      (array1-i12 (f64.4-aref array1 (+ i 12))))
+                  (setf acc1 (sb-simd-avx2:f64.4-fmadd231 acc1 array1-i0
+                                                          (f64.4-aref array2 (+ i 0)))
+                        acc2 (sb-simd-avx2:f64.4-fmadd231 acc2 array1-i4
+                                                          (f64.4-aref array2 (+ i 4)))
+                        acc3 (sb-simd-avx2:f64.4-fmadd231 acc3 array1-i8
+                                                          (f64.4-aref array2 (+ i 8)))
+                        acc4 (sb-simd-avx2:f64.4-fmadd231 acc4 array1-i12
+                                                          (f64.4-aref array2 (+ i 12)))))
              finally (return (f64.4-hsum (f64.4+ acc1 acc2 acc3 acc4))))
        (loop for i of-type fixnum from n0 below n
              summing (* (aref array1 i) (aref array2 i))
@@ -237,7 +242,7 @@
 
 (defun benchmark-simd-sum-double (&rest v-lengths)
   (declare (optimize (speed 3) (safety 0) (debug 0))
-	   (notinline simd-sum1))
+	   (notinline simd-sum))
   (loop for len in v-lengths
 	do (format t "Doing sum of a ~A long double float vector 1e6 times~%" len)
 	collect (let ((u (make-array len :element-type 'double-float
