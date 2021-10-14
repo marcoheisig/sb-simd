50d794bdcecf4392fd4e6daf11761719e7f28559
Marco Heisig <marco.heisig@fau.de>
2021-10-14 10:25:56 +0200

    Simplify the define-types macro.

--- code/define-types.lisp
+++ code/define-types.lisp
@@ -1,14 +1,10 @@
 (in-package #:sb-simd-internals)
 
 (macrolet
-    ((define-type (value-record-name)
-       (with-accessors ((name value-record-name)
-                        (type value-record-type))
-           (find-value-record value-record-name)
-         `(deftype ,name () ',type)))
-     (define-types ()
+    ((define-types ()
        `(progn
           ,@(loop for value-record being the hash-values of *value-records*
                   collect
-                  `(define-type ,(value-record-name value-record))))))
+                  `(deftype ,(value-record-name value-record) ()
+                     ',(value-record-type value-record))))))
   (define-types))
